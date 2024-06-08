4a16cc5473ec11fdef845ef683de349e3e56df6c
bikerski-ctrl <vibikerski@gmail.com>
2024-06-08 16:48:35 +0200

    untested registerview

---
+++ socialmedia/user_auth/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+
+
+class CustomUserCreationForm(UserCreationForm):
+    last_name = forms.CharField(required=False)
+    profile_picture = forms.ImageField(requried=False, allow_empty_file=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({"class": "form-control"})
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2', 'profile_picture']
--- socialmedia/user_auth/views.py
+++ socialmedia/user_auth/views.py
@@ -1,7 +1,22 @@
-from django.views.generic import DetailView
+from django.views.generic import DetailView, CreateView
+from django.http import HttpResponseRedirect
+from django.contrib.auth import login
+from django.urls import reverse_lazy
 from .models import User
+from .forms import CustomUserCreationForm
+from socialmedia.settings import LOGIN_REDIRECT_URL
 
 
 class ProfileView(DetailView):
     model = User
     template_name = 'profile/profile.html'
+
+
+class RegisterView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = "registration/register.html"
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return HttpResponseRedirect(reverse_lazy(LOGIN_REDIRECT_URL))

20dc2ade7d7f80cd5dfbccac5b24870f9ea914d9
bikerski-ctrl <vibikerski@gmail.com>
2024-06-08 23:22:13 +0200

    Navbar, logging out works

--- socialmedia/socialmedia/urls.py
+++ socialmedia/socialmedia/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('user_auth.urls')),
+    path('', TemplateView.as_view(template_name='base.html')),  # temporary main page
 ]
 
 if settings.DEBUG:
--- socialmedia/user_auth/urls.py
+++ socialmedia/user_auth/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import ProfileView, RegisterView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('register', RegisterView.as_view(), name="register"),
+    path('logout', LogoutView.as_view(), name="logout"),
+
+    # MUST BE KEPT LAST
     path('<str:pk>', ProfileView.as_view(), name="profile"),
 ]

35ebc835fd38ab5056aeba4f9a343cb01038ac1a
bikerski-ctrl <vibikerski@gmail.com>
2024-06-09 22:11:37 +0200

    Status changing

--- socialmedia/user_auth/urls.py
+++ socialmedia/user_auth/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import ProfileView, RegisterView
+from .views import ProfileView, RegisterView, update_user_status
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('register', RegisterView.as_view(), name="register"),
-    path('logout', auth_views.LogoutView.as_view(), name="logout"),
+    path('register/', RegisterView.as_view(), name="register"),
+    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
 
     # MUST BE KEPT LAST
-    path('<str:pk>', ProfileView.as_view(), name="profile"),
+    path('<str:pk>/', ProfileView.as_view(), name="profile"),
+    path('<str:pk>/status_update/', update_user_status, name="change_status"),
 ]
--- socialmedia/user_auth/views.py
+++ socialmedia/user_auth/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import DetailView, CreateView
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseBadRequest, HttpResponseForbidden
+from django.urls import reverse_lazy
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from .models import User
 from .forms import CustomUserCreationForm
 from django.conf import settings
@@ -19,3 +21,18 @@ class RegisterView(CreateView):
         user = form.save()
         login(self.request, user)
         return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
+
+
+@login_required
+def update_user_status(request, pk):
+    if request.method != "POST":
+        return HttpResponseBadRequest("Unable to process this request.")
+    user = request.user
+    if pk != user.pk:
+        return HttpResponseForbidden("This action is not allowed.")
+    new_status = request.POST.get("status")
+    user.status = new_status
+    user.save()
+    return HttpResponseRedirect(reverse_lazy(
+        "profile", kwargs={'pk': user.pk}
+    ))

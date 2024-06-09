82a8593f0b44d573443159faa2dd0ae369f631d8
bikerski-ctrl <vibikerski@gmail.com>
2024-06-09 11:33:32 +0200

    Cosmetic changes

--- socialmedia/user_auth/urls.py
+++ socialmedia/user_auth/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from .views import ProfileView, RegisterView
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register', RegisterView.as_view(), name="register"),
-    path('logout', LogoutView.as_view(), name="logout"),
+    path('logout', auth_views.LogoutView.as_view(), name="logout"),
 
     # MUST BE KEPT LAST
     path('<str:pk>', ProfileView.as_view(), name="profile"),

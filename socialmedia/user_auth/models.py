c4ca69f73e7d2c7d8899caca51f4fc38654af676
bikerski-ctrl <vibikerski@gmail.com>
2024-06-17 13:50:39 +0200

    Friendship and subscription

--- socialmedia/user_auth/models.py
+++ socialmedia/user_auth/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinLengthValidator
@@ -29,4 +30,34 @@ class User(AbstractUser):
         blank=True
     )
 
+    friends = models.ManyToManyField("self", through='Friendship')
+
     REQUIRED_FIELDS = ["email", "first_name"]
+
+
+class Subscription(models.Model):
+    subscriber = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="subscriptions", on_delete=models.CASCADE)
+    subscribed_to = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="subscribers", on_delete=models.CASCADE)
+    time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('subscriber', 'subscribed_to')
+
+
+class Friendship(models.Model):
+    user1 = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="friend1")
+    user2 = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="friend2")
+    time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user1', 'user2')
+
+
+class FriendRequest(models.Model):
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="sent_friend_requests", on_delete=models.CASCADE)
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="received_friend_requests", on_delete=models.CASCADE)
+    time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('sender', 'receiver')
+

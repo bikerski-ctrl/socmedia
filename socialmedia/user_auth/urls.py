e24e7cac224c2207f4a9315bd141a8d066501697
bikerski-ctrl <vibikerski@gmail.com>
2024-06-18 14:54:28 +0200

    organized urls + saner names

--- socialmedia/user_auth/urls.py
+++ socialmedia/user_auth/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProfileView, RegisterView, update_user_status, update_user_description
+from .views import ProfileView, RegisterView, SubscriptionView, FriendRequestView, update_user_status, update_user_description, unfriend_user
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -10,4 +10,7 @@ urlpatterns = [
     path('<str:pk>/', ProfileView.as_view(), name="profile"),
     path('<str:pk>/status_update/', update_user_status, name="change_status"),
     path('<str:pk>/change_description/', update_user_description, name="change_description"),
+    path('<str:pk>/subscription', SubscriptionView.as_view(), name="subscription"),
+    path('<str:pk>/friend_request', FriendRequestView.as_view(), name="friend_request"),
+    path('<str:pk>/unfriend', unfriend_user, name="unfriend"),
 ]
